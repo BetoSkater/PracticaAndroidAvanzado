a6ff75eb2f5a1dbd3f0e8a584ba8c65524d52a15
Alberto JR <albertojunquera.ra@gmail.com>
2023-05-18 14:52:32 +0200

    Login Returns a 200 and the token value. Token is stored in SharedPreferences and LoginActivity can be skipped in the second login if the user checked the remember login data checkbox.

--- app/src/main/java/com/albertojr/practicaandroidavanzado/UI/BaseActivity/MainActivity.kt
+++ app/src/main/java/com/albertojr/practicaandroidavanzado/UI/BaseActivity/MainActivity.kt
@@ -1,5 +1,7 @@
 package com.albertojr.practicaandroidavanzado.UI.BaseActivity
 
+import android.content.Context
+import android.content.Intent
 import android.os.Bundle
 import com.google.android.material.snackbar.Snackbar
 import androidx.appcompat.app.AppCompatActivity
@@ -17,6 +19,13 @@ class MainActivity : AppCompatActivity() {
 
     private lateinit var appBarConfiguration: AppBarConfiguration
     private lateinit var binding: ActivityMainBinding
+
+    companion object{
+        fun launch(context: Context){
+            val intent = Intent(context,MainActivity::class.java)
+            context.startActivity(intent)
+        }
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         WindowCompat.setDecorFitsSystemWindows(window, false)
--- app/src/main/java/com/albertojr/practicaandroidavanzado/UI/Login/LoginActivity.kt
+++ app/src/main/java/com/albertojr/practicaandroidavanzado/UI/Login/LoginActivity.kt
@@ -1,10 +1,12 @@
 package com.albertojr.practicaandroidavanzado.UI.Login
 
+import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import androidx.lifecycle.LifecycleOwner
 import com.albertojr.practicaandroidavanzado.R
+import com.albertojr.practicaandroidavanzado.UI.BaseActivity.MainActivity
 import com.albertojr.practicaandroidavanzado.databinding.ActivityLoginBinding
 import com.albertojr.practicaandroidavanzado.databinding.ActivityMainBinding
 
@@ -12,6 +14,8 @@ class LoginActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityLoginBinding
     private val viewModel = LoginViewModel()
+    private val TAG_TOKEN = "MyToken"
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -20,21 +24,58 @@ class LoginActivity : AppCompatActivity() {
         binding = ActivityLoginBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        skipLoginIfTokenExist()
+
         //Retrieving performLogin result
         viewModel.login.observe(this){
             Log.d("LOGIN", it.toString())
+            if(binding.cbSaveData.isChecked){
+                saveTokenInSharedPreferences(it)
+                launchMainActivity()
+            }
         }
 
         binding.btLogin.setOnClickListener{
             //TODO add validations
-
             val email = binding.etEmail.text.toString()
             val password = binding.etPassword.text.toString()
 
-            viewModel.performLogin(email,password)
+            if(!email.isEmpty() && !password.isEmpty()){
+                viewModel.performLogin(email,password)
+
+            }else{
+                binding.etEmail.setError("This field is empty or the email is invalid")
+                //TODO set more validations in case email or password are incorreect
+            }
+
         }
 
     }
     //Method to try to retrieve a stored token from SharedPrefferences
     //TODO save the token in preferences in the companion object from the mainActivity.
+    private fun saveTokenInSharedPreferences(token: String) {
+        getPreferences(Context.MODE_PRIVATE).edit().apply(){
+            putString(TAG_TOKEN, token).apply()
+        }
+    }
+
+    private fun checkIfTokenAlreadyExist():Boolean{
+        getPreferences(Context.MODE_PRIVATE).apply {
+            val token = getString(TAG_TOKEN,"")
+            token?.let {
+                return token.isNotEmpty()
+            }
+        }
+        return false
+    }
+
+    private fun launchMainActivity(){
+        MainActivity.launch(this)
+    }
+
+    private fun skipLoginIfTokenExist(){
+        if(checkIfTokenAlreadyExist()){
+            launchMainActivity()
+        }
+    }
 }
